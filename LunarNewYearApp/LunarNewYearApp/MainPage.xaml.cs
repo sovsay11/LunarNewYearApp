32f8e938cdedf2b542fe02f0de847223e53cdd4b
Noah <sovannarasay11@gmail.com>
2022-05-18 19:44:13 -0600

    Copies code from similar project

--- LunarNewYearApp/LunarNewYearApp/MainPage.xaml.cs
+++ LunarNewYearApp/LunarNewYearApp/MainPage.xaml.cs
@@ -8,11 +8,47 @@ using Xamarin.Forms;
 
 namespace LunarNewYearApp
 {
+    /// <summary>
+    /// MODIFY ALL THIS TO USE ANIMALS INSTEAD
+    /// </summary>
     public partial class MainPage : ContentPage
     {
+        // Dictionary holds image paths and associated birthstones
+        Dictionary<string, string> birthStones = new Dictionary<string, string>()
+        {
+            { "Garnet", "garnet.png"},
+            { "Amethyst", "amethyst.png"},
+            { "Aquamarine", "aquamarine.png"},
+            { "Diamond", "diamond.png"},
+            { "Emerald", "emerald.png"},
+            { "Alexandrite", "alexandrite.png"},
+            { "Ruby", "ruby.png"},
+            { "Peridot", "peridot.png"},
+            { "Sapphire", "sapphire.png"},
+            { "Pink Tourmaline", "pinkTourmaline.png"},
+            { "Topaz", "topaz.png"},
+            { "Blue Topaz", "blueTopaz.png"}
+        };
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Grabs user input from the picker and compares the selected index value
+        /// to the dictionary above. Then displays the associated image and gem name.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PckrMonths_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = PckrMonths.SelectedIndex;
+            string gemName = birthStones.Keys.ElementAt(selectedIndex);
+            string gemPath = birthStones.Values.ElementAt(selectedIndex);
+
+            LblBirthStone.Text = $"{gemName}!";
+            ImgGems.Source = gemPath;
         }
     }
 }
