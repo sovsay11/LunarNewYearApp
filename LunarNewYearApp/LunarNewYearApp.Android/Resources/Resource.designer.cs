6619a68923874beab7c9933951fd379206a3d78b
Noah <sovannarasay11@gmail.com>
2024-09-24 22:12:26 -0600

    Update Resource.designer.cs

--- LunarNewYearApp/LunarNewYearApp.Android/Resources/Resource.designer.cs
+++ LunarNewYearApp/LunarNewYearApp.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace LunarNewYearApp.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.0.26")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.99")]
 	public partial class Resource
 	{
 		
